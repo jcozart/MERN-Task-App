1fef28f5391cea6099a210a7ef514d5604669e7d
jcozart <jordancozart10@gmail.com>
2024-05-27 21:52:24 -0500

    added middleware to the project to protect the routes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { getTasks, setTask, updateTask, deleteTask } = require('../controllers/taskController');
+const { protect } = require('../middleware/authMiddleware');
 
-router.get('/', getTasks)
-router.post('/', setTask)
-router.put('/:id', updateTask)
-router.delete('/:id', deleteTask)
+router.get('/', protect, getTasks)
+router.post('/', protect, setTask)
+router.put('/:id', protect, updateTask)
+router.delete('/:id', protect, deleteTask)
 
 module.exports = router;
